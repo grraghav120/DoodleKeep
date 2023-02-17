6ac983e41ef6804190bde03ade7a0ea0c8707068
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-02-17 21:59:18 +0530

    environment setup

--- src/app/welcome/auth.service.ts
+++ src/app/welcome/auth.service.ts
@@ -1,30 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  onSignUp(requestBody:any){
-    let body={
-      "email":requestBody.emailAdd,
-      "password":requestBody.password,
-      "returnSecureToken":true,
-    }
-    return this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB3mCm-eO4H7rRzLPpAQLZahhZFVsn0p_A',body);
+  onSignUp(requestBody: any) {
+    let body = {
+      email: requestBody.emailAdd,
+      password: requestBody.password,
+      returnSecureToken: true,
+    };
+    return this.http.post(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        environment.fireBaseKey,
+      body
+    );
   }
 
-  onLogin(requestBody:any){
-    let body={
-      "email":requestBody.emailAdd,
-      "password":requestBody.password,
-      "returnSecureToken":true,
-    }
-    return this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB3mCm-eO4H7rRzLPpAQLZahhZFVsn0p_A',body);
+  onLogin(requestBody: any) {
+    let body = {
+      email: requestBody.emailAdd,
+      password: requestBody.password,
+      returnSecureToken: true,
+    };
+    return this.http.post(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+        environment.fireBaseKey,
+      body
+    );
   }
-
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  fireBaseKey: 'AIzaSyB3mCm-eO4H7rRzLPpAQLZahhZFVsn0p_A',
+};
